2d69f728758b4d29f61cb0c963ef2f24d07691d1
Marin Abernethy <maabernethy@gmail.com>
2013-11-06 19:26:27 -0500

    Changes game and user association from habtm to join table through GamePlayer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:username]
-  has_and_belongs_to_many :games
+  has_many :game_players
+  has_many :games, through: :game_players
 
   def online?
     updated_at > 10.minutes.ago
--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131022195406) do
+ActiveRecord::Schema.define(version: 20131106233620) do
+
+  create_table "game_players", force: true do |t|
+    t.integer  "game_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "game_players", ["game_id"], name: "index_game_players_on_game_id"
+  add_index "game_players", ["user_id"], name: "index_game_players_on_user_id"
 
   create_table "games", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  create_table "games_users", id: false, force: true do |t|
-    t.integer "game_id"
-    t.integer "user_id"
-  end
-
-  add_index "games_users", ["game_id", "user_id"], name: "index_games_users_on_game_id_and_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
