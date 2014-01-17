e6026ae42aa81e66b04bcb29b355225409862b5b
Marin Abernethy <maabernethy@gmail.com>
2014-01-17 10:34:19 -0500

    Model dialog working from click of button

--- app/assets/javascripts/ember-app.js
+++ app/assets/javascripts/ember-app.js
@@ -3,6 +3,38 @@ window.App = Ember.Application.create();
 App.ApplicationRoute = Ember.Route.extend({
   model: function() {
     return window.payload;
+  },
+  actions: {
+    openModal: function(modalName, model) {
+      this.controllerFor(modalName).set('model', model);
+      return this.render(modalName, {
+        into: 'application',
+        outlet: 'modal'
+      });
+    },
+
+    closeModal: function() {
+      return this.disconnectOutlet({
+        outlet: 'modal',
+        parentView: 'application'
+      });
+    }
+  }
+});
+
+App.ModalController = Ember.ObjectController.extend({
+  actions: {
+    close: function() {
+      return this.send('closeModal');
+    }
+  }
+});
+
+App.ModalDialogComponent = Ember.Component.extend({
+  actions: {
+    close: function() {
+      return this.sendAction();
+    }
   }
 });
 
